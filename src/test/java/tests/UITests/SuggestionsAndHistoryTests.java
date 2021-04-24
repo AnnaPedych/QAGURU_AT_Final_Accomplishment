7c386f5587897f0b635eb8ecd3356f25b6bb7133
AnnaPedych <apedych@gmail.com>
2021-04-24 17:05:08 +0300

    added new test

---
+++ src/test/java/tests/UITests/SuggestionsAndHistoryTests.java
@@ -0,0 +1,26 @@
+package tests.UITests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.Test;
+import tests.TestBase;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SuggestionsAndHistoryTests extends TestBase {
+
+    @Test
+    public void verifySearchSuggestionAndHistoryTest() {
+
+        open("");
+        $("#small-searchterms").val("jeans");
+        $(".ui-autocomplete li a").shouldHave(Condition.text("Blue Jeans")).click();
+        String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        assertEquals("http://demowebshop.tricentis.com/blue-jeans", currentUrl);
+        $(".product-name h1").shouldHave(Condition.text("Blue Jeans"));
+        open("");
+        $(".block-recently-viewed-products").$("a[href='/blue-jeans']").shouldBe(Condition.visible);
+    }
+}

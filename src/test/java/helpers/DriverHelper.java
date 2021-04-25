f7b0fa5fb2fe58da7febc05802fc30aa2545d09c
AnnaPedych <apedych@gmail.com>
2021-04-25 11:57:23 +0300

    added helpers and required for them dependencies

--- src/test/java/helpers/DriverHelper.java
+++ src/test/java/helpers/DriverHelper.java
@@ -1,35 +1,64 @@
 package helpers;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import config.ConfigHelper;
+import config.DriverConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import static com.codeborne.selenide.Selenide.getWebDriverLogs;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static java.lang.String.join;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class DriverHelper {
-    public static void configureDriver() {
-        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-
-        Configuration.startMaximized = true;
-
-        if (ConfigHelper.isRemoteWebDriver()) {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
-            Configuration.remote = ConfigHelper.getWebRemoteDriver();
-        }
+    private static DriverConfig getDriverConfig() {
+        return ConfigFactory.newInstance().create(DriverConfig.class, System.getProperties());
     }
+
+    public static String getWebRemoteDriver() {
+        // https://%s:%s@selenoid.autotests.cloud/wd/hub/
+        return String.format(getDriverConfig().webRemoteDriverUrl(),
+                getDriverConfig().webRemoteDriverUser(),
+                getDriverConfig().webRemoteDriverPassword());
+    }
+
+    public static boolean isRemoteWebDriver() {
+        return !getDriverConfig().webRemoteDriverUrl().equals("");
+    }
+
+    public static String getVideoUrl() {
+        return getDriverConfig().videoStorage();
+    }
+
+    public static boolean isVideoOn() {
+        return !getVideoUrl().equals("");
+    }
+
     public static String getSessionId(){
         return ((RemoteWebDriver) getWebDriver()).getSessionId().toString().replace("selenoid","");
     }
 
     public static String getConsoleLogs() {
-        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
+        return join("\n", getWebDriverLogs(BROWSER));
+    }
+
+    public static void configureDriver() {
+        addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browser = getDriverConfig().browserName();
+        Configuration.browserVersion = getDriverConfig().browserVersion();
+        Configuration.browserSize = getDriverConfig().browserSize();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (isRemoteWebDriver()) {
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.remote = getWebRemoteDriver();
+        }
+        Configuration.browserCapabilities = capabilities;
     }
 }

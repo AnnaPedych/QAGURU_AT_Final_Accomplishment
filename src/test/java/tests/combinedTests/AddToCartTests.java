aa2d0d835f32dba6b8c18de6454adebcb20581d7
AnnaPedych <apedych@gmail.com>
2021-04-26 19:52:48 +0300

    added allure steps for the first TC

--- src/test/java/tests/combinedTests/AddToCartTests.java
+++ src/test/java/tests/combinedTests/AddToCartTests.java
@@ -14,6 +14,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
@@ -28,32 +29,35 @@ public class AddToCartTests extends TestBase {
     @Test
     @Order(1)
     public void AddToCartAnonymousTests() {
-        Map<String, String> cookies = new Auth().getAnonymousCookies();
-
-        open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
-        getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookies.get("Nop.customer")));
-        getWebDriver().manage().addCookie(new Cookie("ARRAffinity", cookies.get("ARRAffinity")));
-
-        open("");
-        $("a[href='/cart'] .cart-qty").shouldHave(text("(0)"));
-
-        given()
-                .filter(filters().customTemplates())
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookies(cookies)
-                .body("product_attribute_16_5_4=14&product_attribute_16_6_5=15&product_attribute_16_3_6=18&product_attribute_16_4_7=44&product_attribute_16_8_8=22&addtocart_16.EnteredQuantity=1")
-        .when()
-                .post("/addproducttocart/details/16/1")
-        .then()
-                .statusCode(200)
-                .log().body()
-                .body("success", is(true))
-                .body("updatetopcartsectionhtml", equalTo("(1)"))
-                .extract().response();
-
-        open("");
-        $("a[href='/cart'] .cart-qty").shouldHave(text("(1)"));
-
+            Map<String, String> cookies = new Auth().getAnonymousCookies();
+        step("Get cookies", () -> {
+            open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
+            getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookies.get("Nop.customer")));
+            getWebDriver().manage().addCookie(new Cookie("ARRAffinity", cookies.get("ARRAffinity")));
+        });
+        step("Verify that cart is empty", () -> {
+                    open("");
+                    $("a[href='/cart'] .cart-qty").shouldHave(text("(0)"));
+                });
+        step("Add product to cart", () -> {
+                    given()
+                            .filter(filters().customTemplates())
+                            .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                            .cookies(cookies)
+                            .body("product_attribute_16_5_4=14&product_attribute_16_6_5=15&product_attribute_16_3_6=18&product_attribute_16_4_7=44&product_attribute_16_8_8=22&addtocart_16.EnteredQuantity=1")
+                            .when()
+                            .post("/addproducttocart/details/16/1")
+                            .then()
+                            .statusCode(200)
+                            .log().body()
+                            .body("success", is(true))
+                            .body("updatetopcartsectionhtml", equalTo("(1)"))
+                            .extract().response();
+                });
+        step("Check that 1 product is present in the cart", () -> {
+            open("");
+            $("a[href='/cart'] .cart-qty").shouldHave(text("(1)"));
+        });
     }
 
     @Test
@@ -78,9 +82,9 @@ public class AddToCartTests extends TestBase {
                         .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                         .cookies(cookies)
                         .body("product_attribute_16_5_4=14&product_attribute_16_6_5=15&product_attribute_16_3_6=18&product_attribute_16_4_7=44&product_attribute_16_8_8=22&addtocart_16.EnteredQuantity=1")
-                .when()
+                        .when()
                         .post("/addproducttocart/details/16/1")
-                .then()
+                        .then()
                         .statusCode(200)
                         .log().body()
                         .body("success", is(true))

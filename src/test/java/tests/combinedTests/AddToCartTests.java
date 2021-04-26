fe473558ca28183a0a7450850bf9283df784dfdb
AnnaPedych <apedych@gmail.com>
2021-04-26 22:36:08 +0300

    code reformatting

--- src/test/java/tests/combinedTests/AddToCartTests.java
+++ src/test/java/tests/combinedTests/AddToCartTests.java
@@ -27,18 +27,58 @@ public class AddToCartTests extends TestBase {
 
     @Test
     @Order(1)
-    public void AddToCartAnonymousTests() {
-            Map<String, String> cookies = new Auth().getAnonymousCookies();
+    public void addToCartAnonymousTests() {
+        Map<String, String> cookies = new Auth().getAnonymousCookies();
         step("Get cookies", () -> {
             open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
             getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookies.get("Nop.customer")));
             getWebDriver().manage().addCookie(new Cookie("ARRAffinity", cookies.get("ARRAffinity")));
         });
         step("Verify that cart is empty", () -> {
-                    open("");
-                    $("a[href='/cart'] .cart-qty").shouldHave(text("(0)"));
-                });
+            open("");
+            $("a[href='/cart'] .cart-qty").shouldHave(text("(0)"));
+        });
         step("Add product to cart", () -> {
+            given()
+                    .filter(filters().customTemplates())
+                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                    .cookies(cookies)
+                    .body("product_attribute_16_5_4=14&product_attribute_16_6_5=15&product_attribute_16_3_6=18&product_attribute_16_4_7=44&product_attribute_16_8_8=22&addtocart_16.EnteredQuantity=1")
+                    .when()
+                    .post("/addproducttocart/details/16/1")
+                    .then()
+                    .statusCode(200)
+                    .log().body()
+                    .body("success", is(true))
+                    .body("updatetopcartsectionhtml", equalTo("(1)"))
+                    .extract().response();
+        });
+        step("Check that 1 product is present in the cart", () -> {
+            open("");
+            $("a[href='/cart'] .cart-qty").shouldHave(text("(1)"));
+        });
+    }
+
+    @Test
+    @Order(2)
+    public void addToCartLoggedInTests() {
+        Map<String, String> cookies = new Auth().getAuthorizedCookies(getTestUsername(), getTestPassword());
+        step("Get cookies", () -> {
+            open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
+            getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookies.get("Nop.customer")));
+            getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", cookies.get("NOPCOMMERCE.AUTH")));
+            getWebDriver().manage().addCookie(new Cookie("ARRAffinity", cookies.get("ARRAffinity")));
+        });
+        step("Verify that correct user is logged in", () -> {
+            open("");
+            $(".account").shouldHave(text(getTestUsername()));
+        });
+
+        String initialCartValue = new TestBase().getInitialCartCount();
+        int initialCartCount = Integer.parseInt(initialCartValue.substring(1, initialCartValue.length() - 1));
+
+        step("Add product to the cart and verify it is summed to cart product count", () -> {
+            String response =
                     given()
                             .filter(filters().customTemplates())
                             .contentType("application/x-www-form-urlencoded; charset=UTF-8")
@@ -50,55 +90,15 @@ public class AddToCartTests extends TestBase {
                             .statusCode(200)
                             .log().body()
                             .body("success", is(true))
-                            .body("updatetopcartsectionhtml", equalTo("(1)"))
-                            .extract().response();
-                });
-        step("Check that 1 product is present in the cart", () -> {
+                            .body("updatetopcartsectionhtml", is(("(" + (initialCartCount + 1) + ")")))
+                            .extract().response().asString();
+
+            JSONParser parser = new JSONParser();
+            JSONObject JSONResponse = (JSONObject) parser.parse(response);
+            String cartCount = (String) JSONResponse.get("updatetopcartsectionhtml");
+
             open("");
-            $("a[href='/cart'] .cart-qty").shouldHave(text("(1)"));
-        });
-    }
-
-    @Test
-    @Order(2)
-    public void AddToCartLoggedInTests() {
-        Map<String, String> cookies = new Auth().getAuthorizedCookies(getTestUsername(), getTestPassword());
-        step("Get cookies", () -> {
-                    open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
-                    getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookies.get("Nop.customer")));
-                    getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", cookies.get("NOPCOMMERCE.AUTH")));
-                    getWebDriver().manage().addCookie(new Cookie("ARRAffinity", cookies.get("ARRAffinity")));
-                });
-        step("Verify that correct user is logged in", () -> {
-                    open("");
-                    $(".account").shouldHave(text(getTestUsername()));
-                });
-
-        String initialCartValue = new TestBase().getInitialCartCount();
-        int initialCartCount = Integer.parseInt(initialCartValue.substring(1, initialCartValue.length() - 1));
-
-        step("Add product to the cart and verify it is summed to cart product count", () -> {
-                    String response =
-                            given()
-                                    .filter(filters().customTemplates())
-                                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                                    .cookies(cookies)
-                                    .body("product_attribute_16_5_4=14&product_attribute_16_6_5=15&product_attribute_16_3_6=18&product_attribute_16_4_7=44&product_attribute_16_8_8=22&addtocart_16.EnteredQuantity=1")
-                                    .when()
-                                    .post("/addproducttocart/details/16/1")
-                                    .then()
-                                    .statusCode(200)
-                                    .log().body()
-                                    .body("success", is(true))
-                                    .body("updatetopcartsectionhtml", is(("(" + (initialCartCount + 1) + ")")))
-                                    .extract().response().asString();
-
-        JSONParser parser = new JSONParser();
-        JSONObject JSONResponse = (JSONObject) parser.parse(response);
-        String cartCount = (String) JSONResponse.get("updatetopcartsectionhtml");
-
-        open("");
-        $("a[href='/cart'] .cart-qty").shouldHave(text((cartCount)));
+            $("a[href='/cart'] .cart-qty").shouldHave(text((cartCount)));
         });
     }
 }

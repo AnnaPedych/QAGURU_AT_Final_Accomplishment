1bb34fdee3c69a444dbad66cb3efab50b2913bb6
AnnaPedych <apedych@gmail.com>
2021-04-26 22:29:02 +0300

    added allure steps

--- src/test/java/tests/UITests/CommunityPollTests.java
+++ src/test/java/tests/UITests/CommunityPollTests.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Tag("ui")
@@ -20,30 +21,38 @@ public class CommunityPollTests extends TestBase {
     @Order(2)
     public void voteAsLoggedInUserTest() {
         Map<String, String> cookies = new Auth().getAuthorizedCookies("qaguru@qa.guru", "qaguru@qa.guru1");
-
-        open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
-        getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookies.get("Nop.customer")));
-        getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", cookies.get("NOPCOMMERCE.AUTH")));
-        getWebDriver().manage().addCookie(new Cookie("ARRAffinity", cookies.get("ARRAffinity")));
-
-        open("");
-        $$(".poll-options li input").get(TestData.voteOption).click();
-        $(".vote-poll-button").click();
-        $(".poll-results").shouldBe(Condition.visible);
+        step("Get cookies", () -> {
+            open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
+            getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookies.get("Nop.customer")));
+            getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", cookies.get("NOPCOMMERCE.AUTH")));
+            getWebDriver().manage().addCookie(new Cookie("ARRAffinity", cookies.get("ARRAffinity")));
+        });
+        step("Vote", () -> {
+            open("");
+            $$(".poll-options li input").get(TestData.voteOption).click();
+            $(".vote-poll-button").click();
+        });
+        step("Verify vote results are visible", () -> {
+            $(".poll-results").shouldBe(Condition.visible);
+        });
     }
 
     @Test
     @Order(1)
     public void voteAsAnonymousUserTest() {
         Map<String, String> cookies = new Auth().getAnonymousCookies();
-
-        open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
-        getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookies.get("Nop.customer")));
-        getWebDriver().manage().addCookie(new Cookie("ARRAffinity", cookies.get("ARRAffinity")));
-
-        open("");
-        $$(".poll-options li input").get(TestData.voteOption).click();
-        $(".vote-poll-button").click();
-        $(".poll-vote-error").should(Condition.matchText("Only registered users can vote."));
+        step("Get cookies", () -> {
+            open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
+            getWebDriver().manage().addCookie(new Cookie("Nop.customer", cookies.get("Nop.customer")));
+            getWebDriver().manage().addCookie(new Cookie("ARRAffinity", cookies.get("ARRAffinity")));
+        });
+        step("Vote", () -> {
+            open("");
+            $$(".poll-options li input").get(TestData.voteOption).click();
+            $(".vote-poll-button").click();
+        });
+        step("Check validation message is shown", () -> {
+            $(".poll-vote-error").should(Condition.matchText("Only registered users can vote."));
+        });
     }
 }

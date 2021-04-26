4fecaba8377191350a3c08a402403d4cb2e5e0b2
AnnaPedych <apedych@gmail.com>
2021-04-26 22:34:51 +0300

    added allure steps

--- src/test/java/tests/UITests/MainPageTests.java
+++ src/test/java/tests/UITests/MainPageTests.java
@@ -19,22 +19,24 @@ public class MainPageTests extends TestBase {
 
     @Test
     public void checkAllBlocksLoadedTest() {
-        step(" ", () -> open(""));
-        $("img[alt='Tricentis Demo Web Shop']").shouldBe(Condition.visible);
-        $(".header-links-wrapper").shouldBe(Condition.visible);
-        $(".search-box").shouldBe(Condition.visible);
-        $(".header-menu").shouldBe(Condition.visible);
-        $(".leftside-3").shouldBe(Condition.visible);
-        $(".rightside-3").shouldBe(Condition.visible);
-        $(".center-3").shouldBe(Condition.visible);
-        $(".footer").shouldBe(Condition.visible);
+        step("Verify all blocks are visible", () -> {
+            open("");
+            $("img[alt='Tricentis Demo Web Shop']").shouldBe(Condition.visible);
+            $(".header-links-wrapper").shouldBe(Condition.visible);
+            $(".search-box").shouldBe(Condition.visible);
+            $(".header-menu").shouldBe(Condition.visible);
+            $(".leftside-3").shouldBe(Condition.visible);
+            $(".rightside-3").shouldBe(Condition.visible);
+            $(".center-3").shouldBe(Condition.visible);
+            $(".footer").shouldBe(Condition.visible);
+        });
     }
 
     @Test
     @DisplayName("Console log should not contain errors")
     void checkConsoleLogErrorsTest() {
-        step(" ", () -> open(""));
-        step(" ", () -> {
+        step("Open test Url", () -> open(""));
+        step("Verify there are no errors in console log", () -> {
             String consoleLogs = getConsoleLogs();
             assertThat(consoleLogs, not(containsString("SEVERE")));
         });
